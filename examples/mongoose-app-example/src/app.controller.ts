7c6a19eaf5bea51befe45871f52348afb5f74ce7
Chau <ctran2428@gmail.com>
2018-11-27 07:59:39 -0600

    fixed repository link

--- examples/mongoose-app-example/src/app.controller.ts
+++ examples/mongoose-app-example/src/app.controller.ts
@@ -1,20 +1,5 @@
 import { Get, Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AbstractModel } from '../../../dist';
-import { Schema } from 'mongoose';
-
-interface AppModel extends AbstractModel {
-  test: string;
-}
-
-export const appSchema = new Schema(
-  {
-    test: {
-      type: String,
-    },
-  },
-  { timestamps: true },
-);
 
 @Controller()
 export class AppController {
--- examples/mongoose-app-example/src/app.module.ts
+++ examples/mongoose-app-example/src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AbstractModule, ObjectMapping } from 'nest-abstract';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { AbstractModule, ObjectMapping } from '../../../dist';
-
 @Module({
-  imports: [
-    AbstractModule.forRoot({ objectMappingType: ObjectMapping.Mongoose }),
-  ],
+  imports: [AbstractModule.forRoot({objectMappingType: ObjectMapping.Mongoose})],
   controllers: [AppController],
   providers: [AppService],
 })
