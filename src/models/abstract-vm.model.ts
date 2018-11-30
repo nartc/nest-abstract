693cd5f7a267192311c489db64a5a394c8976026
Chau <ctran2428@gmail.com>
2018-11-30 13:59:08 -0600

    update version

--- src/models/abstract-vm.model.ts
+++ src/models/abstract-vm.model.ts
@@ -1,10 +1,10 @@
-import { ApiModelPropertyOptional } from "@nestjs/swagger";
+import { ApiModelPropertyOptional } from '@nestjs/swagger';
 
 export class AbstractVm {
-  @ApiModelPropertyOptional({ type: String, format: "date-time" })
+  @ApiModelPropertyOptional({ type: String, format: 'date-time' })
   createdAt?: Date;
 
-  @ApiModelPropertyOptional({ type: String, format: "date-time" })
+  @ApiModelPropertyOptional({ type: String, format: 'date-time' })
   updatedAt?: Date;
 
   @ApiModelPropertyOptional() id?: string;
